89a5aaaa92fb8356862fbab1d6b14b8a1a11f028
DarioMaza <dmaza1907@gmail.com>
2023-11-06 14:10:16 +0100

    ver productos

--- principal.php
+++ principal.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <?php require './Funciones/conexionTienda.php' ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
     <h1>Principal</h1>
@@ -22,32 +23,32 @@
     <div class="container">
         <table class="table table-striped">
                 <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Usuario</th>
-                    <th>Contraseña</th>
-                    <th>Nombre</th>
-                    <th>Apellidos</th>
-                    <th>Fecha de nacimiento</th>
-                </tr>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Precio</th>
+                        <th>Descripcion</th>
+                        <th>Cantidad en stock</th>                    
+                    </tr>
                 </thead>
-                <tbody>
-            <?php
-            $sql = "SELECT * FROM usuarios";
-            $resultado = $conexion->query($sql);
+            <tbody>
+                <?php
+                $sql = "SELECT * FROM productos";
+                $resultado = $conexion->query($sql);
 
-            while ($row = $resultado -> fetch_assoc()){
-                echo "<tr>";
-                echo "<td>". $row["id"]. "</td>";
-                echo "<td>" . $row["usuario"] . "</td>";
-                echo "<td>" . $row["contrasena"] . "</td>";
-                echo "<td>" . $row["nombre"] . "</td>";
-                echo "<td>" . $row["apellidos"] . "</td>";
-                echo "<td>" . $row["fechaNacimiento"] . "</td>";
-                echo "</tr>";
-            }
-            ?>    
+                while ($row = $resultado -> fetch_assoc()){
+                    echo "<tr>";
+                    echo "<td>". $row["idProducto"]. "</td>";
+                    echo "<td>" . $row["nombreProducto"] . "</td>";
+                    echo "<td>" . $row["precioProducto"] . "</td>";
+                    echo "<td>" . $row["descProducto"] . "</td>";
+                    echo "<td>" . $row["cantidad"] . "</td>";
+                    echo "</tr>";
+                }
+                ?>    
             </tbody>
         </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
